e36b8dbd2f19db93182152f442c4b06b23106a08
Sobrino Julian <sobrinojulian@pm.me>
2021-07-08 20:35:57 -0300

    /blog & posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,32 +1,52 @@
 const fs = require("fs");
+const yaml = require("js-yaml");
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (config) {
-  config.addPassthroughCopy({
-    "node_modules/alpinejs/dist/alpine.js": "static/alpine.js",
-    "./netlifycms.yml": "./admin/config.yml",
-  });
+  // Merge data instead of overriding
+  config.setDataDeepMerge(true);
 
-  // Static
+  config.addPassthroughCopy({
+    "node_modules/alpinejs/dist/alpine.js": "alpine.js",
+    "./netlifycms.yml": "./admin/config.yml",
+    "node_modules/prismjs/themes/prism-tomorrow.css": "prism-tomorrow.css",
+  });
   config.addPassthroughCopy("www/static");
+
+  // human readable date
+  config.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+  // Syntax Highlighting for Code blocks
+  config.addPlugin(syntaxHighlight);
+
+  // To Support .yaml Extension in _data
+  // You may remove this if you can use JSON
+  config.addDataExtension("yaml", (contents) =>
+    yaml.safeLoad(contents)
+  );
 
   // 404 support in `eleventy serve`
   config.setBrowserSyncConfig({
     callbacks: {
-     ready: function(err, bs) {
-       const content_404 = fs.readFileSync('public/404.html');
-       bs.addMiddleware("*", (req, res) => {
-        // Provides the 404 content without redirect.
-        res.write(content_404);
-        res.end();
-      });
-     }
-    }
+      ready: function (err, bs) {
+        const content_404 = fs.readFileSync("public/404.html");
+        bs.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
   });
 
   return {
     dir: {
       input: "www",
-      output: "public"
+      output: "public",
     },
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('daisyui')
+    require('daisyui'),
+    require("@tailwindcss/typography")
   ],
 };
