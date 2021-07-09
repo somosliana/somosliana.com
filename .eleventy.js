6059c981dd900ecb3e40a8fa01ccb5d4ba521c8b
Sobrino Julian <sobrinojulian@pm.me>
2021-07-09 19:54:02 -0300

    Lazy Load + SEO plugins (Test)

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,21 @@
 const minifyHtml = require('./src/minifyHtml')
 const browsersyncConfig = require('./src/browsersyncConfig')
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+const pluginSEO = require("eleventy-plugin-seo");
+
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = function (config) {
   config.addPassthroughCopy({
-    'node_modules/alpinejs/dist/alpine.js': 'alpine.js',
+    './node_modules/alpinejs/dist/alpine.js': 'alpine.js',
     './netlifycms.yml': './admin/config.yml',
-    'node_modules/prismjs/themes/prism-tomorrow.css': 'prism-tomorrow.css',
-    'www/static': 'static'
+    './node_modules/prismjs/themes/prism-tomorrow.css': 'prism-tomorrow.css',
+    './www/static': 'static'
   })
+
+  config.addPlugin(lazyImagesPlugin);
+  config.addPlugin(pluginSEO, require("./www/_data/seo.json"));
 
   if (isProduction) {
     config.addTransform('minifyHtml', minifyHtml)
