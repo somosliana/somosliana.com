b45c7d62a950bbaab03bc77684b4000757f94d1c
Sobrino Julian <sobrinojulian@pm.me>
2021-07-12 17:44:17 -0300

    Sort .eleventy.js, Add filters, plugins, Rename sprite.svg, Fix showing elements until alpine loads

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,36 @@
 const minifyHtml = require('./src/minifyHtml')
 const browsersyncConfig = require('./src/browsersyncConfig')
-const pluginSEO = require("eleventy-plugin-seo");
-//const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
-
-
-const isProduction = process.env.NODE_ENV === 'production'
+const pluginSEO = require('eleventy-plugin-seo')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const yaml = require('js-yaml')
+const { DateTime } = require('luxon')
 
 module.exports = function (config) {
+  // Better defaults
+  config.setDataDeepMerge(true)
+
+  // Extensions
+  config.addDataExtension('yaml', (contents) => yaml.safeLoad(contents))
+
+  // Plugins
+  config.addPlugin(pluginSEO, require('./www/_data/seo.json'))
+  config.addPlugin(syntaxHighlight)
+
+  // Filters
+  config.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy')
+  })
+
+  // Copy files
   config.addPassthroughCopy({
     './node_modules/alpinejs/dist/alpine.js': 'alpine.js',
     './netlifycms.yml': './admin/config.yml',
     './node_modules/prismjs/themes/prism-tomorrow.css': 'prism-tomorrow.css',
-    './www/static': 'static'
+    './www/static': 'static',
   })
 
-  //config.addPlugin(lazyImagesPlugin);
-  config.addPlugin(pluginSEO, require("./www/_data/seo.json"));
-
+  // Conditional configs
+  const isProduction = process.env.NODE_ENV === 'production'
   if (isProduction) {
     config.addTransform('minifyHtml', minifyHtml)
   } else {
@@ -24,9 +38,11 @@ module.exports = function (config) {
   }
 
   return {
+    // Directory structure
     dir: {
       input: 'www',
       output: 'public',
+      layouts: '_layouts'
     },
   }
 }
